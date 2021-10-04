bcc6b219df15946eee2331e45efe8ba45cbe52ec
Paul McKellar <git@paulmckellar.com>
2021-10-04 14:30:53 -0700

    freq

--- csv.rb
+++ csv.rb
@@ -1,4 +1,4 @@
-card_cvs = File.new("./card.csv", 'w')
+card_cvs = File.new("./card_data.csv", 'w')
 
 1.upto(8000).each do |i|
   card = File.new("./cards/#{i}", 'r').read
@@ -6,3 +6,22 @@ card_cvs = File.new("./card.csv", 'w')
   card_cvs << [i.to_s.rjust(4, "0"), data].join(",") + "\n"
 end
 
+card_cvs.close
+
+
+card_freq_file = File.new("./card_frequency.csv", 'w')
+
+card_freq = {}
+1.upto(8000).each do |i|
+  card = File.new("./cards/#{i}", 'r').read
+  card.split("\n")[1,46].map{|x| x.split(",")[2]}.each do |card_title|
+    card_freq[card_title] ||= 0
+    card_freq[card_title] += 1
+  end
+end
+
+card_freq.sort_by{|k,v| v}.reverse.each do |k,v|
+  card_freq_file << "#{k},#{v}" + "\n"
+end
+
+card_freq_file.close
